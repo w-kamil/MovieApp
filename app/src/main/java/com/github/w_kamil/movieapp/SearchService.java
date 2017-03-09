4d39821210155b347686b14d1812795e1c5b2c15
w-kamil <w-kamil@wp.pl>
2017-03-09 18:23:28 +0100

    list activity first

---
+++ app/src/main/java/com/github/w_kamil/movieapp/SearchService.java
@@ -0,0 +1,14 @@
+package com.github.w_kamil.movieapp;
+
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface SearchService {
+
+    @GET("/")
+    Observable<SearchResult> search (@Query("s") String title);
+
+
+}
